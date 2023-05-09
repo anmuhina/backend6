a3314cbc3cc2a8c6e517bbbac253065b9c0307f4
anmuhina <112845722+anmuhina@users.noreply.github.com>
2023-05-09 13:33:14 +0300

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,1 +1,6 @@
-
+<?php
+$user = 'u52811';
+     $pass = '8150350';
+     $db = new PDO('mysql:host=localhost;dbname=u52811', $user, $pass,
+       [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]); 
+?>
