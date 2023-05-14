54695bfd0b2095f01a4b14b271041eaf2a432a80
anmuhina <112845722+anmuhina@users.noreply.github.com>
2023-05-14 15:29:28 +0300

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,3 +1,5 @@
+<body>
+
 <?php
 
 /**
@@ -26,6 +28,107 @@ print('Вы успешно авторизовались и видите защи
 // Реализовать просмотр и удаление всех данных.
 // *********
 
-<form>
+<?php
+if (!empty($messages)) {
+  print('<div id="messages">');
+  foreach ($messages as $message) {
+    print($message);
+  }
+  print('</div>');
+}
+?>
+      
+<form action="" class="forma" method="POST">
+  <?php  
+    foreach($values as $value) {
+    <?php  
+        echo
+    '<label>
+        ID:<br>
+        <input id="data" name="app_id" value="'; print($value['app_id']);  echo '" >
+    </label><br>
+    
+    <label>
+        Имя:<br>
+        <input id="data" name="name" value="'; print ($value['name']); echo '" >
+    </label><br>
+    <label>
+        Email:<br>
+        <input id="data" name="email" type="email" value="'; print($values['email']); echo '" >
+    </label><br>
+    <label>
+        Дата рождения:<br>
+         <select id="data" name="birth_date" >';
+           
+                 $birthdate=$value['birth_date'];
+                 /*if ($value['birth_date']=='') {
+                         for ($i = 1922; $i <= 2022; $i++) {
+                            printf('<option value="%d">%d год</option>', $i, $i);
+                         }
+                 }
+                 else {*/
+                         printf('<option value="%d">%d год</option>', $birthdate, $birthdate);
+                         for ($i = 1922; $i <= 2022; $i++) {
+                            printf('<option value="%d">%d год</option>', $i, $i);
+                         }
+                 //}
+        ; echo 
+        '</select>
+    </label><br>
+          
+    Пол:<br>
+        <div>';
+             
+                /*if ($value['sex']=='') {
+                           print ('<label><input id="data" type="radio" name="sex" value="ж">Ж</label> 
+                           <label><input id="data" type="radio" name="sex" value="м">М</label><br>');
+                }
+                else {*/
+                  echo '<label><input id="data" type="radio" name="sex" value="ж"'; if ($value['sex']=='ж') print('checked="checked"'); echo '>Ж</label> 
+                  <label><input id="data" type="radio" name="sex" value="м"'; if ($values['sex']=='м') print('checked="checked"'); echo '>М</label><br>';       
+                //}
+       echo '</div>
+        
+    Количество конечностей:<br />
+        <div>';
+                /*if ($values['amount_of_limbs']=='') {
+                        print ('<label><input id="data" type="radio" name="amount_of_limbs" value="2"> 2 </label>
+                        <label><input id="data" type="radio" name="amount_of_limbs" value="3"> 3 </label>
+                        <label><input id="data" type="radio" name="amount_of_limbs" value="4"> 4 </label><br>');
+                }
+                else {*/
+                echo
+                       ' <label><input id="data" type="radio" name="amount_of_limbs" value="2"'; if ($value['amount_of_limbs']==2) print('checked="checked"'); echo '> 2 </label>
+                        <label><input id="data" type="radio" name="amount_of_limbs" value="3"'; if ($value['amount_of_limbs']==3) print('checked="checked"'); echo '> 3 </label>
+                        <label><input id="data" type="radio" name="amount_of_limbs" value="4"'; if ($value['amount_of_limbs']==4) print('checked="checked"'); echo '> 4 </label><br>';
+                //} 
+        echo '</div>
+        
+    <label>
+        Сверхспособности:<br>
+        <select id="data" name="abilities[]" multiple="multiple" >';
+                /*if (empty($values['abilities']) || !is_array($values['abilities'])) {
+                        print ('<option value="Бессмертие">Бессмертие</option>
+                        <option value="Прохождение сквозь стены">Прохождение сквозь стены</option>
+                        <option value="Левитация">Левитация</option>');
+                }
+                else {*/
+                echo
+                    '<option value="Бессмертие"'; if (in_array('Бессмертие', $value['abilities'])) {print('selected="selected"');} echo '>Бессмертие</option>
+                     <option value="Прохождение сквозь стены"'; if (in_array('Прохождение сквозь стены', $value['abilities'])) {print('selected="selected"');} echo '>Прохождение сквозь стены</option>
+                     <option value="Левитация"'; if (in_array('Левитация', $value['abilities'])) {print('selected="selected"');} echo '>Левитация</option>';
+                //}
+        echo '</select>
+    </label><br>
+        
+    <label>
+        Биография:<br/>
+        <textarea id="data" name="biography">'; print $value['biography']; echo '</textarea>  
+        </label><br>
+        
+    <div> <input id="sub" name="save" type="submit" value="Сохранить"> </div>
+    <div> <input id="sub" name="delete" type="submit" value="Удалить"> </div>';
+    ?>
 </form>
+</body>
 
