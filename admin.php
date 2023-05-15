79a66e73f7d608400d6729391164809188443a12
anmuhina <112845722+anmuhina@users.noreply.github.com>
2023-05-15 08:40:39 +0300

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,14 +1,13 @@
 <!DOCTYPE html>
 <html lang="ru">
     <head>
+        <link rel="stylesheet" href="style.css" type="text/css"/>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="style.css">
 </head> 
 <body>
 
 <?php
-     
-        include('style.css');
+    
         include('connection.php');
 
 /**
